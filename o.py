c4f0cf296306514c0caf49cb8e74629b009c8f02
İrfan Batuhan Aksu <72353097+Nettrox@users.noreply.github.com>
2024-01-17 16:35:01 +0300

    Update o.py

--- o.py
+++ o.py
@@ -6,6 +6,6 @@ def rep(test_str, O):
     return test_str
  
 
-input_str = "knk benzer bir durum bende de var. olmaması için hayatımızı çöpe atmamış olmamız ve bir şeylerin peşinde koşturuyor olmamız gerek sanırım. ikimiz de ileride evsiz kalacağımızı unutmaya çalışarak yaşıyoruz" #buraya istenen metni ekle
+input_str = "..." #buraya istenen metni ekle
 O = "o"
 print(rep(input_str, O))
